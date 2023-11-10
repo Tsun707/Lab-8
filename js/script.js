811084b22b19e450a11ad17e72c59c8539cb234a
Tsun707 <tsun24@id.iit.edu>
2023-11-09 22:30:08 -0800

    js try out

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 function init(){
-	function textinput(){
-	var textshow = document .getElementById('entryinput').value;
-	document.getElementById('textoutput').innerHTML = textshow;
-	}
+	
 //add your javascrip between these two lines of code
 
 
