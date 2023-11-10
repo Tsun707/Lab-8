285a603df76eac1616d0066c5605aff6866d824f
Tsun707 <tsun24@id.iit.edu>
2023-11-09 21:20:09 -0800

    JS try out

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function init(){
+/function alertfunction(){
+	alert('Tianrui Sun: This is test')
+}
+
+var botton= document.getElementById('entrybutton');
+botton.addEventListener('click', alertfunction);/add your javascrip between these two lines of code
+
+window.addEventListener('load', init);
