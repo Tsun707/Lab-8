c05079dd74a515bcc7a6692575344fd964a8d7ff
Tsun707 <tsun24@id.iit.edu>
2023-11-09 22:24:59 -0800

    JS try out

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 function init(){
-	var textinput = new
+	function textinput(){
+	var textshow = document .getElementById('entryinput').value;
+	document.getElementById('textoutput').innerHTML = textshow;
+	}
 //add your javascrip between these two lines of code
-function alertfunction(){
-	alert('Tianrui Sun: This is test')
-    }
 
-    var botton= document.getElementById('entrybutton');
-    botton.addEventListener('click', alertfunction);
-	
+
+
+
+
+
+
+
+
 window.addEventListener('load', init);
-}
